d3facc1b63e416b05848f22f35b6cf91b971aa27
Jakub <99818101+czessio@users.noreply.github.com>
2025-03-14 03:26:18 +0000

    Delete ml_model/model_server.py

--- ml_model/model_server.py
+++
@@ -1,6 +0,0 @@
-# --------------------------------------------------------------------------------------------
-
-# This file contains the code for the model server. The model server is a simple
-# Flask server that listens for requests on a specified port and responds with
-
-# --------------------------------------------------------------------------------------------
